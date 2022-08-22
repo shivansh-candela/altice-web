c2eeb6aab4914178dc48b9c5944fabb4c2bfacbb
goyalsaurabh06 <saurabh.goyal@candelatech.com>
2022-08-22 19:15:58 +0530

    Added 80mhz testcases
    Signed-off-by: goyalsaurabh06 <saurabh.goyal@candelatech.com>

--- altice/wlan-testing/tests/e2e/basic/performance_tests/wifi_capacity_test/wifi_capacity_channel_country_uni/country_code_US_cli/channel_width_80Mhz/udp_download_1G_single_client/open/fiveg_band/test_throughput_US_80Mhz_tcp_single_client_download_5g_open.py
+++ altice/wlan-testing/tests/e2e/basic/performance_tests/wifi_capacity_test/wifi_capacity_channel_country_uni/country_code_US_cli/channel_width_80Mhz/udp_download_1G_single_client/open/fiveg_band/test_throughput_US_80Mhz_tcp_single_client_download_5g_open.py
@@ -156,3 +156,88 @@ class TestCountryUS80Mhz5G(object):
                 allure.attach.file(source=pdf,
                                    name="WiFi_Capacity_1GBPS_Download_Throughput_udp_5g_Test", attachment_type="PDF")
             assert False
+
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2546", name="WIFI-6938")
+    @pytest.mark.open
+    @pytest.mark.twentyMhz
+    @pytest.mark.fiveg
+    @pytest.mark.channel149
+    @pytest.mark.download
+    def test_client_nat_open_chn36_80Mhz_US_5g_download(self, instantiate_profile, get_lf_logs,
+                                                        lf_test, update_report,
+                                                        station_names_fiveg, lf_tools,
+                                                        test_cases, testbed, al_1, get_configuration):
+        """
+           pytest -m "country_code and twentyMhz and open and fiveg and channel36"
+        """
+        profile_data = setup_params_general["ssid_modes"]["open"][0]
+        ssid_name = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "open"
+        mode = "NAT"
+        band = "fiveg"
+        vlan = 1
+        channel = setup_params_general['rf-5G-5']['5G']['channel']
+        channel_width = setup_params_general['rf-5G-5']['5G']['channel-width']
+        expected_throughput = setup_params_general["expected-throughput"]
+        batch_size = 1
+
+        lf_tools.reset_scenario()
+
+        obj = instantiate_profile(get_configuration['access_point'][0], "../libs/apnos/", "2.x")
+        obj.check_and_set_ap_channel(radio="5G", band=channel_width, channel=channel)
+
+        lf_tools.add_stations(band="5G", num_stations=1, dut=lf_tools.dut_name, ssid_name=ssid_name)
+        # lf_tools.add_stations(band="ax", num_stations=1, dut=lf_tools.dut_name, ssid_name=ssid_name)
+        lf_tools.Chamber_View()
+
+        wct_obj = lf_test.wifi_capacity(instance_name="test_client_open_NAT_udp_dl_5g", mode=mode, vlan_id=vlan,
+                                        download_rate="1Gbps", batch_size="1",
+                                        upload_rate="0", protocol="UDP-IPv4", duration="60000")
+
+        report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+
+        lf_tools.attach_report_graphs(report_name=report_name)
+        lf_tools.attach_report_kpi(report_name=report_name)
+
+        csv_val = lf_tools.read_csv_individual_station_throughput(dir_name=report_name, option=None,
+                                                                  individual_station_throughput=False, kpi_csv=True,
+                                                                  file_name="/kpi.csv", batch_size=str(batch_size))
+        print(csv_val)
+        print(f"Download Traffic Throughput: {csv_val['Down']['DL Mbps - 1 STA']}")
+        actual_throughput = csv_val['Down']['DL Mbps - 1 STA']
+
+        result = {
+
+            "result": None,
+            "ssid-name": ssid_name,
+            "security": security,
+            "security-key": security_key,
+            "band": band,
+            "channel": channel,
+            "description": "WiFi capacity test",
+            "test-download": "1Gbps",
+            "test-batch-size": "1",
+            "test-upload-rate": "0",
+            "test-protocol": "UDP-IPV4",
+            "test-duration": "60 Sec",
+            "expected-throughput": f" > {expected_throughput}",
+            "actual-throughput": actual_throughput
+        }
+
+        if expected_throughput < float(actual_throughput):
+            result["result"] = "PASS"
+            pdf = lf_tools.create_dynamic_pdf(report_name, get_configuration, result)
+            print(f"pdf: {pdf}")
+            if os.path.exists(pdf):
+                allure.attach.file(source=pdf,
+                                   name="WiFi_Capacity_1GBPS_Download_Throughput_udp_5g_Test", attachment_type="PDF")
+            assert True
+        else:
+            result["result"] = "FAIL"
+            pdf = lf_tools.create_dynamic_pdf(report_name, get_configuration, result)
+            print(f"pdf: {pdf}")
+            if os.path.exists(pdf):
+                allure.attach.file(source=pdf,
+                                   name="WiFi_Capacity_1GBPS_Download_Throughput_udp_5g_Test", attachment_type="PDF")
+            assert False
