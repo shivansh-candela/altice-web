e90d18f74d8a2a4d493a147350744b9f29b9c29f
Nikita Yadav <nikita.yadav@candelatech.com>
2023-09-04 17:55:14 +0530

    dfs-132, 133, 134, 135, 136, 137

--- DFS/detection_probability_test.py
+++ DFS/detection_probability_test.py
@@ -412,7 +412,7 @@ class DfsTest(Realm):
             command = f"sudo python3 lf_hackrf_dfs.py --pulse_width {width} --pulse_interval {pri} --pulse_count {count} --sweep_time 1000 --one_burst --freq {freq} --lf_hackrf {self.lf_hackrf}"
         if type == "legacy_w56-1":
             command = f"python3 lf_hackrf_dfs.py --pulse_width {width} --pulse_interval {pri} --pulse_count {count} --tx_sample_rate 2 --sweep_time 1000 --freq {freq} --one_burst --lf_hackrf {self.lf_hackrf}"
-        if type == "FCC0" or type == "FCC1" or type == "FCC2" or type == "FCC3" or type == "FCC4" or type == "KOREA":
+        if type == "FCC0" or type == "FCC1" or type == "FCC2" or type == "FCC3" or type == "FCC4" or type == "KOREA" or type == "W56PULSE":
             command = f"python3 lf_hackrf_dfs.py --rf_type {type},{width},{pri},{count},20 --lf_hackrf {self.lf_hackrf} --freq {freq} --one_burst --log_level debug"
         if type == "w53-3":
             command = f"sudo python3 lf_hackrf_dfs.py --radar_type W53CHIRP,{width},{blank_time},{long_pulse_width},{chirp_width},{prf},{num_con_pair},{freq},20 --one_burst --lf_hackrf {self.lf_hackrf} --log_level debug "
@@ -834,30 +834,77 @@ class DfsTest(Realm):
                         interval_ = 330
                         count_ = 70
                 elif fcc == "Japan-w56-1":
-                    width_ = 1
-                    interval_ = 2778
-                    count_ = 18
+                    if self.legacy == "True":
+                        width_ = 1
+                        interval_ = 2778
+                        count_ = 18
+                    else:
+                        width_ = 0.5
+                        interval_ = 720
+                        count_ = 18
                 elif fcc == "Japan-w56-2":
-                    width_ = 1
-                    interval_ = 1429
-                    count_ = 18
+                    if self.legacy == "True":
+                        width_ = 1
+                        interval_ = 1429
+                        count_ = 18
+                    else:
+                        width_ = 1
+                        interval_ = 700
+                        count_ = 18
                 elif fcc == "Japan-w56-3":
-                    width_ = 2
-                    interval_ = 4000
-                    count_ = 18
+                    if self.legacy == "True":
+                        width_ = 2
+                        interval_ = 4000
+                        count_ = 18
+                    else:
+                        width_ = 2
+                        interval_ = 250
+                        count_ = 18
                 elif fcc == "Japan-w56-4":
-                    width_ = str(random.randint(1, 5))
-                    interval_ = str(random.randint(150, 230))
-                    count_ = str(random.randint(23, 29))
+                    if self.legacy == "True":
+                        width_ = str(random.randint(1, 5))
+                        interval_ = str(random.randint(150, 230))
+                        count_ = str(random.randint(23, 29))
+                    else:
+                        w53_3_width_range = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
+                                             2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5,
+                                             3.6,
+                                             3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0]
+                        random_number = random.choice(w53_3_width_range)
+                        width_ = random_number
+                        interval_ = random.randint(4347, 6667)
+                        count_ = random.randint(23, 29)
                 elif fcc == "Japan-w56-5":
-                    width_ = str(random.randint(6, 10))
-                    interval_ = str(random.randint(200, 500))
-                    count_ = str(random.randint(16, 18))
+                    if self.legacy == "True":
+                        width_ = str(random.randint(6, 10))
+                        interval_ = str(random.randint(200, 500))
+                        count_ = str(random.randint(16, 18))
+                    else:
+                        w53_3_width_range = [6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0]
+                        random_number = random.choice(w53_3_width_range)
+                        width_ = random_number
+                        interval_ = random.randint(2000, 5000)
+                        count_ = random.randint(16, 18)
                 elif fcc == "Japan-w56-6":
-                    width_ = str(random.randint(11, 20))
-                    interval_ = str(random.randint(200, 500))
-                    count_ = str(random.randint(12, 16))
-
+                    if self.legacy == "True":
+                        width_ = str(random.randint(11, 20))
+                        interval_ = str(random.randint(200, 500))
+                        count_ = str(random.randint(12, 16))
+                    else:
+                        w53_3_width_range = [11.0, 11.1, 11.2, 11.3, 11.4, 11.5, 11.6, 11.7, 11.8, 11.9,
+                                            12.0, 12.1, 12.2, 12.3, 12.4, 12.5, 12.6, 12.7, 12.8, 12.9,
+                                            13.0, 13.1, 13.2, 13.3, 13.4, 13.5, 13.6, 13.7, 13.8, 13.9,
+                                            14.0, 14.1, 14.2, 14.3, 14.4, 14.5, 14.6, 14.7, 14.8, 14.9,
+                                            15.0, 15.1, 15.2, 15.3, 15.4, 15.5, 15.6, 15.7, 15.8, 15.9,
+                                            16.0, 16.1, 16.2, 16.3, 16.4, 16.5, 16.6, 16.7, 16.8, 16.9,
+                                            17.0, 17.1, 17.2, 17.3, 17.4, 17.5, 17.6, 17.7, 17.8, 17.9,
+                                            18.0, 18.1, 18.2, 18.3, 18.4, 18.5, 18.6, 18.7, 18.8, 18.9,
+                                            19.0, 19.1, 19.2, 19.3, 19.4, 19.5, 19.6, 19.7, 19.8, 19.9,
+                                            20.0]
+                        random_number = random.choice(w53_3_width_range)
+                        width_ = random_number
+                        interval_ = random.randint(2000, 5000)
+                        count_ = random.randint(12, 16)
                 if fcc == "FCC5":
                     print(burst_)
                     main_dict[fcc][var_1]["Burst"] = burst_
@@ -1053,6 +1100,11 @@ class DfsTest(Realm):
                                 self.run_hackrf(type="legacy_w56-1", width=width_, pri=interval_, count=count_, freq=str(frequency[str(self.channel)]))
                             else:
                                 self.run_hackrf(type="legacy", width=width_, pri=interval_, count=count_, freq=str(frequency[str(self.channel)]))
+                        else:
+                            var = "W56PULSE"
+                            self.run_hackrf(type=var, width=width_, pri=interval_, count=count_,
+                                            freq=str(frequency[str(self.channel)]))
+
                     else:
                         self.run_hackrf(width=width_, pri=interval_, count=count_,
                                         freq=str(frequency[str(self.channel)]))
