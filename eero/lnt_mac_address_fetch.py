174b857200bfdd222840f88a880bc8a4aade75a3
tarun-candela <tarunkumar.madabathula@candelatech.com>
2023-11-29 17:56:05 +0530

    lnt_mac_address_fetch.py : CANDELA-285 Updated the summary for the script & added comments
    Signed-off-by: tarun-candela <tarunkumar.madabathula@candelatech.com>

--- eero/lnt_mac_address_fetch.py
+++ eero/lnt_mac_address_fetch.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
+"""
+This script will fetch the mac address for the stations from the lanforge.
 
+Capabilities:
+    * The script will give the .csv file output in the output report directory.
+    * The .csv file will contain the mac address for all existing stations in respective lanforge system.
+
+Pre-requisites:
+    * Any lanforge system with already existing stations.
+    * Since the script is dependent on the lanforge-scripts, the script should be place "lanforge-scripts/py-scripts/" path to get the output.
+
+    CLI (Command Line Interface) to run the script:
+        python3 large_network_test.py --mgr 192.168.200.240 --scenario 200-clients-long-run
+        --twog_radio 1.1.wiphy2 1.1.wiphy3 1.1.wiphy4 1.1.wiphy5
+        --fiveg_radio 1.1.wiphy0 1.1.wiphy1 1.3.wiphy0 1.3.wiphy1 1.3.wiphy2 1.3.wiphy3 1.3.wiphy4
+        --sixg_radio 1.4.wiphy3 1.4.wiphy4 1.4.wiphy5 1.4.wiphy6 1.4.wiphy7 --twog_channel 1 --fiveg_channel 36 --sixg_channel 37
+        --twog_sniff_radio 1.4.wiphy0 --fiveg_sniff_radio 1.4.wiphy1 --sixg_sniff_radio 1.4.wiphy2 --increment 10
+        --csv_outfile sta_mac_list.csv --attenuators 1.1.1031 1.1.3104 --attenuator_module_values 0,0,0,0 0,0,0,0
+
+"""
 import sys
 import os
 import csv
@@ -29,6 +48,7 @@ class LNT_MAC_FETCHING(Realm):
         self.lf_host = lf_host
         self.lf_port = lf_port
 
+    # building the output folder, where the output .csv file will be saved
     def building_output_directory(self, directory_name="LNT_STA_MAC_LIST"):
         now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S_")  # %Y-%m-%d-%H-h-%m-m-%S-s
         if directory_name:
@@ -41,6 +61,7 @@ class LNT_MAC_FETCHING(Realm):
             print("ERROR : The report path is existed but unable to find. Exception raised : {}\n".format(e))
         return self.directory
 
+    # querying the port-mgr tab for mac address & writing them in a .csv file
     def station_mac_listing_to_csv(self):
         resp = self.json_get(f"/port/?fields=mac")
         filtered_data = []
